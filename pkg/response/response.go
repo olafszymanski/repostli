5cbf442a1a28c97193f53207637b115a01080a7c
Olaf Szymański <olaf.szym123@gmail.com>
2023-02-05 23:33:11 +0100

    Improve response (#13)

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -6,41 +6,22 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
-type Response interface {
-	Empty() (events.APIGatewayProxyResponse, error)
-	WithBody(body map[string]string) (events.APIGatewayProxyResponse, error)
-	WithError(err error) (events.APIGatewayProxyResponse, error)
-}
-
 type response struct {
 	statusCode int
 	headers    map[string]string
 	body       string
 }
 
-func New(statusCode int, headers map[string]string) Response {
-	return &response{
-		statusCode: statusCode,
-		headers:    headers,
-		body:       "",
+type Options func(options *response) error
+
+func New(options ...Options) (events.APIGatewayProxyResponse, error) {
+	r := &response{}
+	for _, opt := range options {
+		err := opt(r)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
 	}
-}
-
-func (r *response) Empty() (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: r.statusCode,
-		Headers:    r.headers,
-		Body:       r.body,
-	}, nil
-}
-
-func (r *response) WithBody(body map[string]string) (events.APIGatewayProxyResponse, error) {
-	b, err := json.Marshal(body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	r.body = string(b)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: r.statusCode,
@@ -49,19 +30,42 @@ func (r *response) WithBody(body map[string]string) (events.APIGatewayProxyRespo
 	}, nil
 }
 
-func (r *response) WithError(err error) (events.APIGatewayProxyResponse, error) {
-	e, merr := json.Marshal(map[string]string{
-		"error": err.Error(),
-	})
-	if merr != nil {
-		return events.APIGatewayProxyResponse{}, merr
+func WithStatusCode(statusCode int) Options {
+	return func(options *response) error {
+		options.statusCode = statusCode
+		return nil
 	}
+}
 
-	r.body = string(e)
+func WithHeaders(headers map[string]string) Options {
+	return func(options *response) error {
+		options.headers = headers
+		return nil
+	}
+}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: r.statusCode,
-		Headers:    r.headers,
-		Body:       r.body,
-	}, nil
+func WithBody(body map[string]string) Options {
+	return func(options *response) error {
+		b, err := json.Marshal(body)
+		if err != nil {
+			return err
+		}
+
+		options.body = string(b)
+		return nil
+	}
+}
+
+func WithError(err error) Options {
+	return func(options *response) error {
+		e, err := json.Marshal(map[string]string{
+			"error": err.Error(),
+		})
+		if err != nil {
+			return err
+		}
+
+		options.body = string(e)
+		return nil
+	}
 }
