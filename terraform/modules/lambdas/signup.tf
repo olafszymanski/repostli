9c559457fe78c89d0592f8e70679c36bbca1df96
Olaf Szymanski <olaf.szym123@gmail.com>
2023-02-05 19:51:23 +0100

    Fix lambda definition

--- terraform/modules/lambdas/signup.tf
+++ terraform/modules/lambdas/signup.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "signup" {
-  name = "signup-role-${title(var.environment)}"
+  name = "SignUp-${title(var.environment)}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -18,9 +18,10 @@ resource "aws_lambda_function" "signup" {
   function_name = "signup-${var.environment}"
   role          = aws_iam_role.signup.arn
 
-  source_code_hash = filebase64sha256("../../../build/signup.zip")
-  filename         = "../../../build/signup.zip"
-  runtime          = "go1.2x"
+  source_code_hash = filebase64sha256("../../build/signup.zip")
+  filename         = "../../build/signup.zip"
+  handler          = "signup"
+  runtime          = "go1.x"
 
   timeout = 10
 }
